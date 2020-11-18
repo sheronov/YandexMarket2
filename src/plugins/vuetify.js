1a02a66e1d9d4a566cfe6150e3808fa1c9ec1166
sheronov <webrush@bk.ru>
2020-11-18 22:41:41 +0300

    added routes, icons and temp ajax tree

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,3 +1,4 @@
+import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import 'vuetify/src/styles/styles.sass'
@@ -5,4 +6,7 @@ import 'vuetify/src/styles/styles.sass'
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi', // default
+    },
 });
