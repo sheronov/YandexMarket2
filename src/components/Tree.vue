c1af37cf2e2f41dcdce217d3d0e2ae1b10b68c43
sheronov <webrush@bk.ru>
2021-01-08 22:03:13 +0300

    поправил автораскрытие дерева при выбранных категориях

--- src/components/Tree.vue
+++ src/components/Tree.vue
@@ -3,15 +3,14 @@
     <v-row>
       <v-col>
         <v-treeview
+            :items="categories"
+            :load-children="loadCategories"
+            :item-text="itemText"
+            :item-key="itemKey"
             class="yandexmarket-categories"
             ref="categoriesTree"
-            :load-children="loadCategories"
-            :items="categories"
-            :open.sync="opened"
             selection-type="independent"
             item-children="children"
-            item-text="text"
-            item-key="pk"
             open-on-click
             hoverable
             dense
@@ -20,10 +19,10 @@
             <v-simple-checkbox
                 v-if="item.selectable"
                 color="primary"
-                class="ml-1"
+                class="ml-2"
                 @click.stop=""
                 @input="selectCategory($event, item)"
-                :value="selected.indexOf(item.pk) !== -1"
+                :value="selected.indexOf(item[itemKey]) !== -1"
                 :ripple="false"
             ></v-simple-checkbox>
           </template>
@@ -61,49 +60,37 @@ import api from "@/api";
 export default {
   name: 'Tree',
   data: () => ({
+    itemKey: 'pk',
+    itemText: 'text',
     loading: false,
     categories: [],
     selected: [],
-    opened: []
+    opened: [],
+    loaded: [],
   }),
   methods: {
     selectCategory(value, category) {
       if (value) {
-        this.selected.push(category.pk);
+        this.selected.push(category[this.itemKey]);
       } else {
-        this.selected = this.selected.filter(selected => selected !== category.pk);
+        this.selected = this.selected.filter(selected => selected !== category[this.itemKey]);
       }
     },
     loadCategories(item) {
       this.loading = true;
-      const parentNode = this.$refs.categoriesTree.nodes[item.pk];
-      console.log(item, parentNode);
       return api.post('mgr/getcategories', {id: item.id})
           .then(({data}) => {
-            // if (open) {
-            //   parentNode.isOpen = true;
-            // }
             item.children = data;
-            data.forEach((node) => {
-              // this.$refs.categoriesTree.nodes[node.pk] = {...parentNode, item: node, vnode: null};
-              if (node.selected) {
-                this.selected.push(node.pk);
+            data.forEach((child) => {
+              if (child.selected) {
+                this.selected.push(child[this.itemKey]);
               }
-              if (node.expanded) {
-
-                // console.log('node expanded', JSON.stringify(node));
-                this.$nextTick().then(() => {
-                  parentNode.isOpen = true;
-                  this.loadCategories(node);
-                  // setTimeout(() => {
-                  //   this.opened.push(node.pk)
-                  // }, 100);
-                });
-                // setTimeout(() => {this.opened.push(node.pk)}, 100);
-                // this.$nextTick(() => this.loadCategories(node, true));
+              if (child.expanded) {
+                setTimeout(() => {
+                  this.$refs.categoriesTree.updateOpen(child[this.itemKey], true);
+                  // we have to wait before the tree created children nodes
+                }, 50);
               }
-
-
             });
           })
           .catch(e => console.error(e))
@@ -111,21 +98,15 @@ export default {
     },
   },
   mounted() {
+    this.loading = true;
     api.post('mgr/getcategories', {id: 'root'})
-        .then(({data}) => {
-          this.categories = data;
-          data.forEach(node => {
-            if (node.expanded) {
-              // setTimeout(() => {
-              //   this.opened.push(node.pk)
-              // }, 1000);
-              this.$nextTick().then(() => this.opened.push(node.pk));
-              // setTimeout(() => this.loadCategories(node, true), 10);
-              // this.$nextTick(() => this.loadCategories(node, true))
-            }
-          })
-        })
-        .catch(e => console.error(e));
+        .then(({data}) => this.categories = data)
+        .catch(e => console.error(e))
+        .then(() => {
+          this.loading = false
+          // by default all context are opening
+          this.$nextTick().then(() => this.$refs.categoriesTree.updateAll(true));
+        });
   }
 }
 </script>
@@ -135,7 +116,9 @@ export default {
   display: none;
 }
 
-.yandexmarket-categories >>> .v-treeview-node__label .v-treeview-node__content {
+.yandexmarket-categories >>> .v-treeview-node__content {
   margin-left: 0;
 }
+
+
 </style>
