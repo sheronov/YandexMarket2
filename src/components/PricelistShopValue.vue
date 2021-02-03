3681555debe77169222e0844addf4e4a8c40c038
sheronov <webrush@bk.ru>
2021-02-04 02:34:32 +0300

    fixed frontend shop fields

---
+++ src/components/PricelistShopValue.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="yandexmarket-shop-value mb-3">
+    <!-- TODO: сделать поддержку разных компонентов -->
+    <v-select
+        v-if="value.type === 4"
+        :label="value.label"
+        :value="value.column"
+        :items="value.properties.values"
+        filled
+        dense
+        multiple
+        chips
+        deletable-chips
+        small-chips
+        hide-details="auto"
+        item-value="key"
+        @change="inputField($event)"
+    >
+      <template v-slot:selection="{ attrs, index, item, selected }">
+        <v-chip
+            v-bind="attrs"
+            :input-value="selected"
+            text-color="white"
+            :color="!index ? 'primary' : 'grey'"
+            :title="!index ? 'Основная валюта' : 'Выбрать основной'"
+            @click.stop="makeFirst(item)"
+            @click:close="removeChip(item)"
+            close
+            small
+        >
+          <strong>{{ item.key }}</strong>
+          <span class="pl-1">({{ item.text }})</span>
+        </v-chip>
+      </template>
+    </v-select>
+    <v-text-field
+        v-else
+        :label="value.label"
+        :value="value.column"
+        hide-details="auto"
+        filled
+        dense
+        @change="inputField($event)"
+    >
+      <template v-slot:append v-if="value.help">
+        <v-tooltip bottom :max-width="400" :close-delay="500">
+          <template v-slot:activator="{ on }">
+            <v-icon v-on="on">
+              icon-question-circle
+            </v-icon>
+          </template>
+          <div class="text-caption" style="white-space: pre-line;">{{ value.help }}</div>
+        </v-tooltip>
+      </template>
+    </v-text-field>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PricelistShopValue',
+  props: {
+    id: {},
+    value: {required: true},
+    marketplace: {required: true},
+    // field: {required: true, type: Object}
+  },
+  computed: {
+    label() {
+      let label = this.value.name;
+      let lexicon = `ym_${this.marketplace}_shop_${this.value.name}`;
+      if (lexicon !== this.$t(lexicon)) {
+        label = this.$t(lexicon);
+      } else {
+        lexicon = `ym_${this.marketplace}_${this.value.name}`;
+        if (lexicon !== this.$t(lexicon)) {
+          label = this.$t(lexicon);
+        }
+      }
+
+      if (this.value.properties && this.value.properties.required) {
+        label += ' *';
+      }
+      return label;
+    },
+    help() {
+      return '';
+    }
+  },
+  methods: {
+    inputField(value) {
+      this.$emit('input', {...this.value, column: value});
+    },
+    removeChip(item) {
+      let values = [...this.value.column];
+      values.splice(values.indexOf(item.key), 1)
+      this.inputField(values);
+    },
+    makeFirst(item) {
+      let values = [...this.value.column];
+      values.splice(values.indexOf(item.key), 1);
+      values.unshift(item.key);
+      this.inputField(values);
+    }
+  }
+}
+</script>
+
+<style>
+.yandexmarket-shop-value {
+  position: relative;
+}
+</style>
--- src/pages/PriceListShop.vue
+++ src/pages/PriceListShop.vue
@@ -2,14 +2,17 @@
   <div class="yandexmarket-pricelist-settings">
     <h4>Настройки магазина</h4>
     <p class="mb-2">Обязательные поля отмечены звёздочкой. Пустые поля не попадут в выгрузку.</p>
-    <pricelist-field
-        v-for="field in fields"
-        :key="field.key"
-        :field="field"
-        :value="form[field.key]"
-        @input="changedValue(field, $event)"
+
+    <pricelist-shop-value
+        v-for="(value,key) in values"
+        :key="key"
+        :value="value"
+        :id="key"
+        :marketplace="pricelist.type"
+        @input="changedValue(key,$event)"
     />
     <p>
+      Тут нужно сделать добавление новых полей и перемещение порядка
       Тут будет название, компания, урл, платформа, версия, агентство, емайл, валюта, показывать ли скидки, доставка
     </p>
   </div>
@@ -17,22 +20,35 @@
 
 <script>
 
-import PricelistField from "@/components/PricelistField";
+// import PricelistField from "@/components/PricelistField";
+import PricelistShopValue from "@/components/PricelistShopValue";
 
 export default {
   name: 'PriceListShop',
-  components: {PricelistField},
+  components: {PricelistShopValue},
   props: {
     pricelist: {type: Object, required: true}
   },
   data: () => ({
     fields: [],
     currencies: [],
-    form: {}
+    form: {},
+    values: {}
   }),
+  watch: {
+    'pricelist.values.shop': {
+      immediate: true,
+      handler: function (pricelistValues) {
+        this.values = {
+          ...this.values,
+          ...pricelistValues
+        }
+      }
+    }
+  },
   methods: {
     previewXml() {
-      this.$emit('preview:xml', 'shop', this.form)
+      this.$emit('preview:xml', 'shop', this.values)
     },
     save() {
       this.$emit('@input:settings', this.form);
@@ -40,8 +56,9 @@ export default {
     reset() {
       this.form = {...this.pricelist.shop || {}};
     },
-    changedValue(field, value) {
-      this.$set(this.form, field.key, value);
+    changedValue(id, value) {
+      // console.log(id, value);
+      this.$set(this.values, id, value);
       this.previewXml();
     }
   },
