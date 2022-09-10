029ec47589484259e4110cf074cbc75d8af33a10
sheronov <webrush@bk.ru>
2022-09-10 23:40:27 +0300

    fixed plugin

--- core/components/yandexmarket2/elements/plugins/yandexmarket2.php
+++ core/components/yandexmarket2/elements/plugins/yandexmarket2.php
@@ -1,24 +1,29 @@
 <?php
-/** @var \MODX\Revolution\modX $modx */
-
-/** @var \MODX\Revolution\modResource $resource */
+/** @var MODX\Revolution\modX|modX $modx */
+/** @var MODX\Revolution\modResource|modResource $resource */
 /** @var array $scriptProperties */
-
 
 switch ($modx->event->name) {
     case 'OnDocFormSave':
         $corePath = $modx->getOption('yandexmarket2_core_path', null,
             $modx->getOption('core_path').'components/yandexmarket2/');
-        // $modx->addPackage('yandexmarket2', $corePath.'Model/');
+        $isMODX3 = class_exists('MODX\Revolution\modX');
+        if (!$isMODX3) {
+            $modx->addPackage('yandexmarket2', $corePath.'model/');
+        }
 
-        $q = $modx->newQuery(YandexMarket\Model\YmPricelist::class);
-        $q->where([
-            'class'            => $resource->class_key,
-            'active'           => 1,
-            'generate_mode:!=' => 0
-        ]);
-        if ($modx->getCount(YandexMarket\Model\YmPricelist::class, $q)) {
-            foreach ($modx->getIterator(YandexMarket\Model\YmPricelist::class, $q) as $ymPricelist) {
+        $q = $modx->newQuery($isMODX3 ? YandexMarket\Model\YmPricelist::class : YmPricelist::class)
+            ->where([
+                'class'            => $resource->class_key,
+                'active'           => 1,
+                'generate_mode:!=' => 0
+            ]);
+        if ($modx->getCount($q->getClass(), $q)) {
+            if (!$isMODX3) {
+                require_once $corePath.'vendor/autoload.php';
+            }
+
+            foreach ($modx->getIterator($q->getClass(), $q) as $ymPricelist) {
                 $pricelist = new \YandexMarket\Models\Pricelist($modx, $ymPricelist);
                 (new \YandexMarket\QueryService($pricelist, $modx))->handleResourceChanges($resource);
             }
